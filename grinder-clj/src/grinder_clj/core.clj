b5d3f5fb923dce72a94847546145c57b8e3ee35f
Alexei Matveev <alexei.matveev@gmail.com>
2017-12-21 23:37:21 +0100

    AM: point to grinder docs

--- grinder-clj/src/grinder_clj/core.clj
+++ grinder-clj/src/grinder_clj/core.clj
@@ -5,8 +5,12 @@
   "I don't do a whole lot."
   [& args]
   (println "Hello, World!")
-  ;; Java class main takes a string array which is not the same as
-  ;; Clojure vector of strings. You dont need to supply anything to
-  ;; Console/main, actually:
+  ;;
+  ;; Java class main()  takes a string array which is  not the same as
+  ;; Clojure vector  of strings. You  dont need to supply  anything to
+  ;; Console/main, actually [1]:
+  ;;
+  ;; [1] http://grinder.sourceforge.net/g3/getting-started.html
+  ;;
   (let [string-array (into-array String args)]
     (net.grinder.Console/main string-array)))
