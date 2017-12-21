ce4e4941f60527a06634006dd05867ca2d586f5b
Alexei Matveev <alexei.matveev@gmail.com>
2017-12-21 23:35:39 +0100

    AM: this starts grinder Console

--- grinder-clj/src/grinder_clj/core.clj
+++ grinder-clj/src/grinder_clj/core.clj
@@ -1,6 +1,12 @@
-(ns grinder-clj.core)
+(ns grinder-clj.core
+  (:import [net.grinder.Console]))
 
 (defn -main
   "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
+  [& args]
+  (println "Hello, World!")
+  ;; Java class main takes a string array which is not the same as
+  ;; Clojure vector of strings. You dont need to supply anything to
+  ;; Console/main, actually:
+  (let [string-array (into-array String args)]
+    (net.grinder.Console/main string-array)))
