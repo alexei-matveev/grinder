4b967d712b9d484e2fe6c5cd2fb70ed8554d6812
Alexei Matveev <alexei.matveev@gmail.com>
2017-12-22 14:55:11 +0100

    AM: exclusions as advised

--- grinder-clj/project.clj
+++ grinder-clj/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.sf.grinder/grinder "3.12-SNAPSHOT"]]
+                 [net.sf.grinder/grinder "3.12-SNAPSHOT"
+                  :exclusions [org.clojure/tools.macro]]
+                 [org.clojure/tools.macro "0.1.5"]]
   :main grinder-clj.core
   ;; Implicit AOT of main is deprecated:
   :aot [grinder-clj.core])
