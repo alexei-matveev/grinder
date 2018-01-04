242fc5ebb9876c4bf5e38bf4198869eae1a1380a
Alexei Matveev <alexei.matveev@gmail.com>
2018-01-04 23:04:40 +0100

    AM: workaround for instrumentation

--- grinder-clj/project.clj
+++ grinder-clj/project.clj
@@ -11,4 +11,9 @@
                  [clj-http "3.7.0"]]
   :main grinder-clj.core
   ;; Implicit AOT of main is deprecated:
-  :aot [grinder-clj.core])
+  :aot [grinder-clj.core]
+  ;; Make Uberjar re-usable for  intrumentation. With Uberjar there by
+  ;; definition just one JAR, wheather you like it or not:
+  :manifest {"Premain-Class" "net.grinder.util.weave.agent.ExposeInstrumentation"
+             "Can-Redefine-Classes" "true"
+             "Can-Retransform-Classes" "true"})
